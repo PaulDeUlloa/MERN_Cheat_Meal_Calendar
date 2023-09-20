65d972723c104b1e11a4b9a7ac32eba5bcaad601
Paul De Ulloa <pauld143@gmail.com>
2023-09-19 23:25:39 -0700

    edits to server folder

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const port = process.env.PORT;
+
+// //* Import mongoose.config
+// require("./config/mongoose.config.js");
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// //* import routes
+// require("./routes/meals.routes")(app);
+
+// //* Listening on port
+// app.listen(port, () => console.log(`✨Listening on port: ${port}✨`));
