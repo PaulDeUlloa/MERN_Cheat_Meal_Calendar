dae15122122288babd5552b359d9de6bf80c9423
Paul De Ulloa <pauld143@gmail.com>
2023-09-22 00:34:56 -0700

    formatting edits

--- client/src/views/CreateMeal.jsx
+++ client/src/views/CreateMeal.jsx
@@ -1,65 +1,71 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate, } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
-
 const CreateMeal = () => {
-    const[name, setName] = useState("");
-    const[date, setDate] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
 
-    const [errors, setErrors] = useState([])
+  const [errors, setErrors] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        axios.post(`http://localhost:8000/api/meals`, {name, date})
-        .then(() => navigate("/meals"))
-        .catch((err)=>{
-            const errResponse = err.response.data.errors;
-            const errArr = [];
-            for(const key in errResponse){
-                errArr.push(errResponse[key].message)
-            }
-            setErrors(errArr);
-        });
-    };
+    axios
+      .post(`http://localhost:8000/api/meals`, { name, date })
+      .then(() => navigate("/meals"))
+      .catch((err) => {
+        const errResponse = err.response.data.errors;
+        const errArr = [];
+        for (const key in errResponse) {
+          errArr.push(errResponse[key].message);
+        }
+        setErrors(errArr);
+      });
+  };
 
-
-
-    return <div>
-        <h2>Add a new meal:</h2>
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>Name: </label>
-                <input 
-                type="text" 
-                value={name} 
-                onChange={(e)=>setName(e.target.value)}
-                />
-                <br />
-                <label>Select Date: </label>
-                <input 
-                type="date"
-                value={date} 
-                onChange={(e)=>setDate(e.target.value)}
-                />
-                <div>
-                    <Link to="/meals">
-                        <button>Cancel</button>
-                    </Link>
-                    {" "}
-                    <input type="submit" value="Submit" />
-                </div>
-            </div>
-        </form>
-        {errors.map((err, idx) =>{
-            return <p key={idx}>{err}</p>;
-        })}
+  return (
+    <div>
+      <h2 class="m-5">Add Delicacy:</h2>
+      <form class="form-control" onSubmit={submitHandler}>
+        <div class="row mb-3">
+          <label class="col-sm-2 col-form-label">Name: </label>
+          <div class="col-sm-8">
+            <input
+              placeholder="Meal name"
+              class="form-control"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-2 col-form-label">Select Date: </label>
+          <div class="col-sm-8">
+            <input
+              class="form-control"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <Link to="/meals">
+            <button className="btn-btn light">Cancel</button>
+          </Link>{" "}
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      {errors.map((err, idx) => {
+        return <p key={idx}>{err}</p>;
+      })}
     </div>
+  );
 };
 
 export default CreateMeal;
--- client/src/views/UpdateMeal.jsx
+++ client/src/views/UpdateMeal.jsx
@@ -1,81 +1,84 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams, } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
-
 const UpdateMeal = () => {
-    const[name, setName] = useState("");
-    const[date, setDate] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [errors, setErrors] = useState([])
+  const [errors, setErrors] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        axios
-        .get(`http://localhost:8000/api/meals/${id}`)
-        .then((res) =>{
-            const oneMeal = res.data;
-            setName(oneMeal.name);
-            setDate(oneMeal.date);
-        })
-        .catch((err) => console.log(err));
-        
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/meals/${id}`)
+      .then((res) => {
+        const oneMeal = res.data;
+        setName(oneMeal.name);
+        setDate(oneMeal.date);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        axios
-        .put(`http://localhost:8000/api/meals/${id}`, {name})
-        .then(() => navigate("/meals"))
-        .catch((err)=>{
-            const errResponse = err.response.data.errors;
-            const errArr = [];
-            for(const key in errResponse){
-                errArr.push(errResponse[key].message)
-            }
-            setErrors(errArr);
-        });
-    };
+    axios
+      .put(`http://localhost:8000/api/meals/${id}`, { name })
+      .then(() => navigate("/meals"))
+      .catch((err) => {
+        const errResponse = err.response.data.errors;
+        const errArr = [];
+        for (const key in errResponse) {
+          errArr.push(errResponse[key].message);
+        }
+        setErrors(errArr);
+      });
+  };
 
-
-
-    return <div>
-        <h2>Edit this meal:</h2>
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>Name: </label>
-                <br></br>
-                <input 
-                type="text" 
-                value={name} 
-                onChange={(e)=>setName(e.target.value)}
-                />
-                <label>Select Date: </label>
-                <br></br>
-                <input 
-                type="date"
-                value={date} 
-                onChange={(e)=>setDate(e.target.value)}
-                />
-                <div>
-                    <Link to="/meals">
-                        <button>Cancel</button>
-                    </Link>
-                    {" "}
-                    <input type="submit" value="Submit" />
-                </div>
-            </div>
-        </form>
-        {errors.map((err, idx) =>{
-            return <p key={idx}>{err}</p>;
-        })}
+  return (
+    <div>
+      <h2>Edit this meal:</h2>
+      <form class="form-control" onSubmit={submitHandler}>
+        <div class="row mb-3">
+          <label class="col-sm-2 col-form-label">Name: </label>
+          <div class="col-sm-8">
+            <input
+              placeholder="Meal name"
+              class="form-control"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-2 col-form-label">Select Date: </label>
+          <div class="col-sm-8">
+            <input
+              class="form-control"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <Link to="/meals">
+            <button className="btn-btn light">Cancel</button>
+          </Link>{" "}
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      {errors.map((err, idx) => {
+        return <p key={idx}>{err}</p>;
+      })}
     </div>
+  );
 };
 
 export default UpdateMeal;
