2729f3af4bf328bccdb270bd5dbc0285264e1188
Paul De Ulloa <pauld143@gmail.com>
2023-09-22 00:43:35 -0700

    formatting and styling of table

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,62 +1,68 @@
-import React,{useState, useEffect} from "react";
-import axios from"axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-    const [mealList, setMealList] = useState([]);
+  const [mealList, setMealList] = useState([]);
 
-    useEffect(() => {
-        axios
-        .get(`http://localhost:8000/api/meals`)
-        .then((res) => setMealList(res.data))
-        .catch((err) => console.log(err));
-        
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/meals`)
+      .then((res) => setMealList(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const handleDelete = (deletedId) =>{
-        axios.delete(`http://localhost:8000/api/meals/${deletedId}`)
-        .then(() => {
-            removeFromDom(deletedId);
-        })
-        .catch((err) => console.log(err));
-    };
-    //* updates dom without refresh
-    const removeFromDom = (deletedId) => {
-        const filteredList = mealList.filter(
-            (eachMeal, idx) => eachMeal._id !== deletedId
-        );
-        setMealList(filteredList);
-    }
+  const handleDelete = (deletedId) => {
+    axios
+      .delete(`http://localhost:8000/api/meals/${deletedId}`)
+      .then(() => {
+        removeFromDom(deletedId);
+      })
+      .catch((err) => console.log(err));
+  };
+  //* updates dom without refresh
+  const removeFromDom = (deletedId) => {
+    const filteredList = mealList.filter(
+      (eachMeal, idx) => eachMeal._id !== deletedId
+    );
+    setMealList(filteredList);
+  };
 
-    return (
-        <div>
-            <h2>Cheat Meal Counter:</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Meal Name: </th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mealList.map((oneMeal, idx) => {
-                    return(
-                        <tr key={idx}>
-                            <td>{oneMeal.name}</td>
-                            <td>
-                                <Link to={`/meals/${oneMeal._id}/edit`}>
-                                    <button>Edit</button></Link>
-                                <Link>
-                                    <button onClick={()=>handleDelete(oneMeal._id)}>Delete</button>
-                                </Link>
-                            </td>
-                        </tr>
-                    )
-                })}
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Cheat Meal Counter: 4</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Meal Name: </th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mealList.map((oneMeal, idx) => {
+            return (
+              <tr key={idx}>
+                <td>{oneMeal.name}</td>
+                <td>
+                  <Link to={`/meals/${oneMeal._id}/edit`}>
+                    <button class="btn-btn light">Edit</button>
+                  </Link>
+                  <Link>
+                    <button
+                      class="btn-btn light"
+                      onClick={() => handleDelete(oneMeal._id)}
+                    >
+                      Delete
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Dashboard;
